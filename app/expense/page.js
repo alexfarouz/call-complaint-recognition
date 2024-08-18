adb11365cd85a94ef03d013ef1256fbeacca9d27
therealsid19 <siddyboy2003@gmail.com>
2024-08-18 01:46:31 -0400

    Homepage and user page

---
+++ app/expense/page.js
@@ -0,0 +1,11 @@
+'use client'
+import UserPage from '../components/UserPage';
+import ExpenseTrack from '../components/ExpenseTrack';
+
+export default function Home() {
+  return (
+    <div>
+      <ExpenseTrack />  
+    </div>
+  );
+};
---
+++ app/profile/page.js
@@ -0,0 +1,10 @@
+'use client'
+import UserPage from '../components/UserPage';
+
+export default function Home() {
+  return (
+    <div>
+      <UserPage />  
+    </div>
+  );
+}
